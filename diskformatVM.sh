d938e0ad455448ea806f97c2ca9882fa1864f9dd
Jeremy Corbeil <jeremy.corbeil@gmail.com>
2026-02-28 15:12:23 -0500

    extended content of disk prep script

--- diskformatVM.sh
+++ diskformatVM.sh
@@ -22,11 +22,16 @@ parted -s -a optimal $1 mkpart primary fat32 2048s 1G
 
 # Create root partition
 echo "Creating linux partition on rest of free space..."
-parted -s -a optimal $1 mkpart primary ext4 1G 100%
+parted -s -a optimal $1 mkpart primary ext4 1G 95%
 
 # Set esp on efi partition
 echo "Setting esp flag on EFI partition..."
 parted -s $1 set 1 esp on
+
+###****
+CURRENTLY UNTESTED FROM THIS POINT
+###****
+
 
 #####################
 ## Disk encryption ##
@@ -34,18 +39,54 @@ parted -s $1 set 1 esp on
 
 # Encrypt root partition
 echo "Encrypt root partition with LUKS2 aes-512..."
-###cryptsetup --label crypt --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 1000 --use-random luksFormat /dev/${DISK}p2
+cryptsetup --label crypt --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 1000 --use-random luksFormat $12
 
 # Open encrypted partition
 echo "Opening crypt partition..."
-###cryptsetup open --allow-discards --type luks /dev/${DISK}p2 root
+cryptsetup open --allow-discards --type luks $12 cryptroot
 
-######################
-## Filesystem setup ##
-######################
+##########################
+## Setup Main Volume LVM##
+##########################
 
-# Make filesystems
-echo "Creating filesystems..."
-###mkfs.fat -F 32 -n EFI /dev/${DISK}p1
-###mkfs.ext4 -L ROOT /dev/mapper/root
+# Make root partition into an LV group
+echo "creating logical volume group on root partition..."
+vgcreate cryptroot /dev/mapper/cryptroot
+echo "creating root logical volume..."
+lvcreate --name root -L 10G cryptroot
+echo "creating swap logical volume..."
+lvcreate --name swap -L 4G cryptroot
+echo "creating home logical volume..."
+lvcreate --name home -l 80%FREE cryptroot
 
+echo "Creating EFI filesystem FAT32..."
+mkfs.fat -F 32 -n EFI $11
+#mkfs.ext4 -L ROOT /dev/mapper/cryptroot
+
+echo "creating root filesystem ext4..."
+mkfs.ext4 -L root /dev/cryptroot/root
+echo "creating swap filesystem..."
+mkswap /dev/cryptroot/swap
+echo "mounting swap volume..."
+swapon /dev/cryptroot/swap
+echo "creating home filesystem ext4..."
+mkfs.ext4 -L home /dev/cryptroot/home
+
+########################################
+## Mount Volumes to Target Filesystem ##
+########################################
+
+# Swap is already mounted, but EFI, root and home still need to be mounted
+
+# mount root and home
+echo "mounting root to target filesystem..."
+mount /dev/cryptroot/root /mnt
+
+echo "mounting home to target filesystem..."
+mkdir -p /mnt/home
+mount /dev/cryptroot/home /mnt/home
+
+# since the intention is to use an EFI stub for boot, only create a /mnt/boot folder and mount to EFI partition
+echo "mounting EFI stub directory..."
+mkdir -p /mnt/boot
+mount $11 /mnt/boot
